9c1bfcb00a78bf07252de394fd373cea195f13ee
Chama_Bro <83635238+Chama-Bro@users.noreply.github.com>
2022-01-12 12:41:30 +0530

    Create Package-Update.py

---
+++ Package-Update.py
@@ -0,0 +1,77 @@
+# Created By Chama Bro
+import os
+
+from time import sleep
+from sys import stdout
+
+y=("   ")
+p=("                          ")
+x=("                             ")
+sp=("")
+
+print (y,"____            _                     _   _           _       _")
+print (y,"|  _ \ __ _  ___| | _____  __ _  ___  | | | |_ __   __| | __ _| |_ ___")
+print (y,"| |_) / _` |/ __| |/ / _ \/ _` |/ _ \ | | | | '_ \ / _` |/ _` | __/ _ \ ")
+print (y,"|  __/ (_| | (__|   <  __/ (_| |  __/ | |_| | |_) | (_| | (_| | ||  __/")
+print (y,"|_|   \__,_|\___|_|\_\___|\__, |\___|  \___/| .__/ \__,_|\__,_|\__\___|")
+print (y,"                          |___/             |_|")
+print (x," _____           _")
+print (x,"|_   _|__   ___ | |")
+print (x,"  | |/ _ \ / _ \| |")
+print (x,"  | | (_) | (_) | |")
+print (x,"  |_|\___/ \___/|_|")
+
+print (sp)
+print (sp)
+z = [p, "Tool Created by Chama Bro" ]
+for x in z:
+        for y in x:
+                print(y,end="")
+                stdout.flush()
+                sleep(0.06)
+
+print (sp)
+
+print ('\033[0;32;40m\n')
+os.system("pkg install python2")
+
+print ('\033[0;31;40m\n')
+os.system("pkg install git")
+
+print ('\033[0;32;40m\n')
+
+os.system("pkg install sl")
+
+print ('\033[0;31;40m\n')
+os.system("pkg install cmatrix")
+
+print ('\033[0;32;40m\n')
+os.system("pkg install clang")
+
+print ('\033[0;31;40m\n')
+os.system("pkg install perl")
+
+print ('\033[0;32;40m\n')
+os.system("pkg install php")
+
+print ('\033[0;31;40m\n')
+os.system("pkg install ruby")
+
+print ('\033[0;32;40m\n')
+os.system("pkg install nmap")
+
+print ('\033[0;31;40m\n')
+os.system("pkg install nano")
+
+print ('\033[0;36;40m\n')
+os.system("pkg update -y && pkg upgrade -y")
+
+print ('\033[0;34;40m\n')
+print (p,"  ____ ")
+print (p," |  _ \  ___  _ __   __")
+print (p," | | | |/ _ \| _ \ / _ \ ")
+print (p," | |_| | (_) | | | |  __/")
+print (p," |____/ \___/|_| |_|\___| ")
+print (sp)
+print (sp)
+print (sp)
